815ea71eec1b67212b7c8a43889238b0f1d8b9b1
Saaleh <97705117+SPoovathumkadavil@users.noreply.github.com>
2023-08-29 13:38:08 -0500

    mm

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,8 +1,74 @@
 package frc.robot;
 
+import edu.wpi.first.math.geometry.Translation3d;
+import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.RobotBase;
+import swervelib.math.Matter;
+import swervelib.parser.PIDFConfig;
 
 public class Constants {
+
+	public static class SwerveModuleConstants {
+
+		public static final double[] kTurnPID = { 0.8, 0, 0 };
+		public static final double kNEOSteerP = 3.0;
+
+		/*
+		 * {Static Gain, FeedForward, Proportional Gain, ModuleID for Tuning}
+		 */
+		public static final double[] kFrontLeftTuningVals = {
+			0.015,
+			0.19,
+			0.15,
+			0,
+		};
+		public static final double[] kFrontRightTuningVals = {
+			0.015,
+			0.19,
+			0.15,
+			1,
+		};
+		public static final double[] kBackLeftTuningVals = {
+			0.015,
+			0.19,
+			0.15,
+			2,
+		};
+		public static final double[] kBackRightTuningVals = {
+			0.015,
+			0.19,
+			0.15,
+			3,
+		};
+	}
+
+	public static class SwerveConstants {
+
+		public static final double ROBOT_MASS = 125 * 0.453592; // 32lbs * kg per pound
+		public static final Matter CHASSIS = new Matter(
+			new Translation3d(0, 0, Units.inchesToMeters(8)),
+			ROBOT_MASS
+		);
+		public static final double LOOP_TIME = 0.10; // velocity lag and command loop time (seconds)
+
+		public static final PIDFConfig xAutoPID = new PIDFConfig(0.7, 0, 0);
+		public static final PIDFConfig yAutoPID = new PIDFConfig(0.7, 0, 0);
+		public static final PIDFConfig angleAutoPID = new PIDFConfig(
+			0.4,
+			0,
+			0.01
+		);
+
+		public static final double MAX_SPEED = 4;
+		public static final double MAX_ACCELERATION = 2;
+
+		// Hold time on motor brakes when disabled
+		public static final double WHEEL_LOCK_TIME = 10; // seconds
+
+		// Joystick Deadband
+		public static final double LEFT_X_DEADBAND = 0.01;
+		public static final double LEFT_Y_DEADBAND = 0.01;
+	}
 
 	public static class TelemetryConstants {
 
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,10 +1,110 @@
 package frc.robot;
 
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.wpilibj.RobotBase;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.button.CommandJoystick;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import frc.robot.Constants.SwerveConstants;
+import frc.robot.commands.baseCommands.AbsoluteDrive;
+import frc.robot.commands.baseCommands.AbsoluteFieldDrive;
+import frc.robot.commands.baseCommands.TeleopDrive;
+import frc.robot.subsystem.swerve.Swerve;
 
 public class RobotContainer {
 
-	public RobotContainer() {} // TODO: Create Constructor Methods
+	Swerve swerve = new Swerve();
+
+	CommandJoystick operatorController = new CommandJoystick(1);
+	CommandXboxController driveController = new CommandXboxController(0);
+
+	public RobotContainer() {
+		configureBindings();
+
+		// Applies deadbands and inverts controls because joysticks
+		// are back-right positive while robot
+		// controls are front-left positive
+		AbsoluteDrive closedAbsoluteDrive = new AbsoluteDrive(
+			swerve,
+			() ->
+				MathUtil.applyDeadband(
+					driveController.getLeftY(),
+					SwerveConstants.LEFT_Y_DEADBAND
+				),
+			() ->
+				MathUtil.applyDeadband(
+					driveController.getLeftX(),
+					SwerveConstants.LEFT_X_DEADBAND
+				),
+			() -> -driveController.getRightX(),
+			() -> -driveController.getRightY(),
+			false
+		);
+
+		AbsoluteFieldDrive closedFieldAbsoluteDrive = new AbsoluteFieldDrive(
+			swerve,
+			() ->
+				MathUtil.applyDeadband(
+					driveController.getLeftY(),
+					SwerveConstants.LEFT_Y_DEADBAND
+				),
+			() ->
+				MathUtil.applyDeadband(
+					driveController.getLeftX(),
+					SwerveConstants.LEFT_X_DEADBAND
+				),
+			() -> driveController.getRightTriggerAxis(),
+			false
+		);
+		TeleopDrive simClosedFieldRel = new TeleopDrive(
+			swerve,
+			() ->
+				MathUtil.applyDeadband(
+					driveController.getLeftY(),
+					SwerveConstants.LEFT_Y_DEADBAND
+				),
+			() ->
+				MathUtil.applyDeadband(
+					driveController.getLeftX(),
+					SwerveConstants.LEFT_X_DEADBAND
+				),
+			() -> driveController.getRawAxis(2),
+			() -> true,
+			false,
+			true
+		);
+		TeleopDrive closedFieldRel = new TeleopDrive(
+			swerve,
+			() ->
+				MathUtil.applyDeadband(
+					operatorController.getY(),
+					SwerveConstants.LEFT_Y_DEADBAND
+				),
+			() ->
+				MathUtil.applyDeadband(
+					operatorController.getX(),
+					SwerveConstants.LEFT_X_DEADBAND
+				),
+			() -> -operatorController.getRawAxis(3),
+			() -> true,
+			false,
+			true
+		);
+
+		swerve.setDefaultCommand(
+			!RobotBase.isSimulation()
+				? closedAbsoluteDrive
+				: closedFieldAbsoluteDrive
+		);
+	}
+
+	void configureBindings() {
+		driveController
+			.a()
+			.toggleOnTrue(new InstantCommand(() -> swerve.zeroGyro()));
+	}
 
 	public Command getAutonomousCommand() {
 		return null; // TODO: CHANGE
